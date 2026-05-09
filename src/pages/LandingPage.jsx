8b467c4470e4d413367771d5dc77223ad1d799e6
sigma700 <196065961+sigma700@users.noreply.github.com>
2026-05-09 12:59:13 +0300

    fixed footer route problem

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -6,8 +6,9 @@ import {useAddToCartStore} from "../stores/addToCartStore";
 import {bufferToDataURL} from "../utils/displayImage";
 import {Link, useNavigate} from "react-router-dom";
 import NavBar from "../components/navBar";
-import Footer from "../components/Footer";
+
 import PremiumLoader from "../components/loader";
+import Footer from "../components/Footer";
 
 const GOLD = "#4A8C2A";
 const GOLD_LIGHT = "#72B84A";
